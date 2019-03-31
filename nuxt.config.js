f179ffd307dacbd87c856748f32fccb62223fe2c
大坪 裕二 <1ds11097s@gmail.com>
2019-03-31 19:17:15 +0900

    topページ調整

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: '出会い系アプリを実際に使ってのレビュー、本当に会えるの？',
+    title: 'マッチングアプリを実際に使ってのレビュー、本当に会えるの？',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
