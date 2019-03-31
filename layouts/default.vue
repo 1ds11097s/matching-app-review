c3230db3b3b719b2d236b244cd34644ec7b8be99
大坪 裕二 <1ds11097s@gmail.com>
2019-03-31 18:58:12 +0900

    topページ作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,81 @@
 <template>
   <div>
-    <nuxt />
+    <header>
+      <h1 class="title">出会い系アプリを実際に使ってのレビュー、本当に会えるの？</h1>
+      <h2 class="subtitle">～実際に使ってみて会えるアプリ、会えないアプリを紹介します！～</h2>
+    </header>
+    <main class="container">
+      <hr>
+      <p>こんにちは！管理人のゆうと申します！</p>
+      <p>当サイトは出会い系アプリを使ってみて本当に会えるアプリはあるのか？</p>
+      <p>会えるならどのアプリが会えるのかを私自身の体験談を元に紹介していくサイトです。</p>
+      <p>このサイトを訪れたあなたも恐らく出会い系アプリについて調べているところだったのではないでしょうか？</p>
+      <p>「出会い系アプリって色々あるけどどれだったら会えるのかな？」</p>
+      <p>「サクラや業者が多いってよく聞くけど本当に会える人はいるのかな？」</p>
+      <p>「危ない事件に巻き込まれることもあるんじゃないかな？」</p>
+      <p>「かわいい子と会うことってできるのかな」</p>
+      <p>等色々な疑問があるのではないかと思います。</p>
+      <p>そんな疑問を解決するためにこのサイトを立ち上げました！</p>
+      <p>あなたの疑問を解決するのはもちろん私自身が使ったアプリからどのアプリならちゃんと会えるのかを紹介していきたいと思います。</p>
+      <p>私自身も出会い系アプリを使い始めたのは2019年の1月からなので今管理人はどのアプリでどのぐらい会えているのかも随時更新していきたいと思います！</p>
+      <p>このアプリを試してほしい、〇〇はどうなのか？等の疑問等もあれば可能な限り対応していきますのでコメントをお待ちしています！</p>
+      <p>このサイトを訪れたあなたも一緒に最高の出会いを目指して頑張って行きましょう！</p>
+      <span>目次</span>
+      <ul>
+       <li>管理人の自己紹介</li>
+       <li>管理人の今のアプリ進捗状況</li>
+       <li>実際に出会えたアプリ紹介</li>
+        <ul>
+         <li>pairs</li>
+         <li>ハッピーメール</li>
+        </ul>
+       <li>まだ出会えていないアプリ紹介</li>
+        <ul>
+         <li>YYC</li>
+         <li>with</li>
+         <li>omiai</li>
+        </ul>
+       </li>
+       <li>あまり出会えなさそうなアプリ紹介</li>
+        <ul>
+         <li>タップる誕生</li>
+         <li>クロスミー</li>
+         <li>poiboy</li>
+         <li>pcmax</li>
+        </ul>
+       </li>
+       <li>絶対に出会えないアプリ紹介(笑)</li>
+        <ul>
+         <li>SF</li>
+         <li>そくでき</li>
+         <li>ももちゃ</li>
+         <li>夜フレ</li>
+        </ul>
+      </ul>  
+      <nuxt/>
+    </main>
   </div>
 </template>
 
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Source Sans Pro", 游ゴシック体, 'Yu Gothic', YuGothic, 'ヒラギノ角ゴシック Pro', 'Hiragino Kaku Gothic Pro', メイリオ, Meiryo, sans-serif;
   font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
 }
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
+h1, h2, h3 {
+  font-weight: normal;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
+.title {
   color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+  margin: 100px 0 30px;
+  text-align: center;
 }
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.subtitle {
+  color: #35495e;
+  text-align: center;
+}
+.container {
+  width: 80%;
+  margin: 0 auto;
 }
 </style>
