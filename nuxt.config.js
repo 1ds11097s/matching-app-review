ab75a7183de1109b2963fbc030852141213afa2a
大坪 裕二 <1ds11097s@gmail.com>
2019-06-09 13:14:42 +0900

    amp

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,17 @@
 import pkg from './package'
 
+const modifyHtml = (html) => {
+  // Add amp-custom tag to added CSS
+  html = html.replace(/<style data-vue-ssr/g, '<style amp-custom data-vue-ssr')
+  // Remove every script tag from generated HTML
+  html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
+  // Add AMP script before </head>
+  const ampScript = '<script async src="https://cdn.ampproject.org/v0.js"></script>'
+  const ampSocial = '<script async custom-element="amp-social-share" src="https://cdn.ampproject.org/v0/amp-social-share-0.1.js"></script>'
+  html = html.replace('</head>', ampScript + ampSocial + '</head>')
+  return html
+}
+
 export default {
   mode: 'universal',
 
@@ -48,6 +60,13 @@ export default {
       }
     ]
   ],
+
+  hooks: {
+    // This hook is called before rendering the html to the browser
+    'render:route': (url, page) => {
+      page.html = modifyHtml(page.html)
+    }
+  },
 
   /*
   ** Build configuration
